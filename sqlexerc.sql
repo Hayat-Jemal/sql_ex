26fbff998270c7d97d3d33aa4b3fe41c01fb65c4
Hayat Jemal <htjemal2000@gmail.com>
2022-07-06 05:40:25 +0300

    add questions

--- sqlexerc.sql
+++ sqlexerc.sql
@@ -15,9 +15,17 @@ LIMIT 6;  --it returs the first 6 results
 
 SELECT *
 FROM customer
-WHERE first_name LIKE 'T%' AND last_name LIKE '%t' OR (first_name LIKE 'B%' OR last_name LIKE'%b' )
+WHERE first_name LIKE 'T%' AND last_name LIKE '%t' OR (first_name LIKE 'B%' OR last_name LIKE'%b' );
 
+--What are the d/t rental durations that the store allows?
+SELECT DISTINCT rental_duration
+FROM film
+ORDER BY 1 DESC;
 
-
-
+--What are the IDs of the last 3 customers return a rental?
+SELECT customer_id, return_date
+FROM rental
+WHERE return_date IS NOT NULL
+  ORDER BY return_date DESC
+  LIMIT 3;
 
